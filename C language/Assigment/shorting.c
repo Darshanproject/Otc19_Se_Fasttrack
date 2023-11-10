0d9e10ac0dcd09cb51e83736f3cdc44ade40c2a1
Darshanproject <gadadarshan6184@gmail.com>
2023-11-10 15:28:39 +0530

    Task 10-11-2023

---
+++ C language/Assigment/shorting.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+    int j,i,arr[7];
+
+    int temp;
+    for(i=0;i<7;i++){
+        printf("Enter your values here : ");
+        scanf("%d",&arr[i]);
+    }
+    printf("Original array: ");
+    for (i = 0; i < 7; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    for ( i = 0; i < 7 - 1; i++) {
+        for (j = 0; j < 7 - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\nSorted array: ");
+    for (i = 0; i < 7; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
